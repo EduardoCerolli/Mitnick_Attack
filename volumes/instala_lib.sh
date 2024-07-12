9a367702d1004215806fb4fddf81219cded926f7
EduardoCerolli <educerolli@gmail.com>
2024-07-12 13:09:09 -0300

    cria scrpit para instalar lib netfilterqueue

---
+++ volumes/instala_lib.sh
@@ -0,0 +1,4 @@
+# instala a lib NetfilterQueue usada para capturar as mensagens da rede
+apt-get update
+apt-get install build-essential python3-dev libnetfilter-queue-dev
+pip3 install NetfilterQueue
